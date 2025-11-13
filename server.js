d94d61159a06a3aa93059ee2b23cbd3ea1147b2c
Nathan Cobut <n.cobut@gmail.com>
2025-11-13 23:17:31 +0100

    Ajout de la configuration HTTPS, des sessions et des dépendances Sequelize et express-session ; mise à jour des formulaires de connexion et d'inscription.

--- server.js
+++ server.js
@@ -3,35 +3,43 @@ var session = require('express-session');
 var path = require('path');
 var bodyParser = require("body-parser");
 var app = express();
+var https = require('https');
 var fs = require('fs');
+var sequelize = require('sequelize');
+const { noDoubleNestedGroup } = require('sequelize/lib/utils/deprecations');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: false
-}))
+  secret: "secret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { 
+    path: '/', 
+    httpOnly: true, 
+    maxAge: 3600000,
+  }
+}));
+app.use(express.static(path.join(__dirname, 'static')));
+
 
 
 app.get('/', function(req, res, next) {
   res.render("layout", {
     title: "Acceuil",
     page: "pages/index",
-    username: "Anonyme"
+    username: req.session.username,
   })
 });
 
 app.get('/login', function(req, res, next) {
-  password = req.query.password;
-
-  if(password == "123pass") { // Si le met de passe est correcte
+  if(req.session.username != undefined) {
     res.render("layout", {
       title: "Acceuil",
       page: "pages/index",
-      username: req.query.username
+      username: req.session.username,
     })
   }
 
@@ -40,18 +48,36 @@ app.get('/login', function(req, res, next) {
       title: "Connection",
       page: "pages/login",
   
-      username: req.query.username,
-      password: password
+      username: "",
+      usernameInput: req.query.username,
     })
   }
   
+});
+
+app.post('/login', function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+
+  if (password === "123pass") {
+    req.session.username = username;
+    res.redirect('/');
+  } else {
+    res.render("layout", {
+      title: "Connection",
+      page: "pages/login",
+      username: "",
+      usernameInput: username,
+    });
+  }
 });
 
 app.get('/signup', function(req, res, next) {
   res.render("layout", {
     title: "Inscription",
     page: "pages/signup",
-    username: "Anonyme"
+    username: req.session.username,
   })
 });
 
@@ -59,10 +85,15 @@ app.get('/incident-creation', function(req, res, next) {
   res.render("layout", {
     title: "Signalement d’incident",
     page: "pages/incident-creation",
-    username: "Anonyme"
+    username: req.session.username,
   })
 });
 
-app.listen(8080, function() {
+
+https.createServer({
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem'),
+  passphrase: 'ingi'
+}, app).listen(8080, function() {
   console.log('Server is running...');
 });
